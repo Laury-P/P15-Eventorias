cc02b8ada27c991d486bc4aa0c443c70f8b4a723
Laury PRIN <prinlauryy@gmail.com>
2026-05-26 17:19:40 +0200

    Config gradle prêt

--- app/src/main/java/com/openclassroom/eventorias/MainActivity.kt
+++ app/src/main/java/com/openclassroom/eventorias/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+
 import com.openclassroom.eventorias.ui.theme.EventoriasTheme
 
 class MainActivity : ComponentActivity() {
