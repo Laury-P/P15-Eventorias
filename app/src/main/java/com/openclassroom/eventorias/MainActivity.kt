dd66f7bfe94150f332f0b247c813eebb924e0f3d
Laury PRIN <prinlauryy@gmail.com>
2026-05-27 16:31:11 +0200

    Ajout d'un splashscreen

--- app/src/main/java/com/openclassroom/eventorias/MainActivity.kt
+++ app/src/main/java/com/openclassroom/eventorias/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 import com.openclassroom.eventorias.core.ui.theme.EventoriasTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,6 +20,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
