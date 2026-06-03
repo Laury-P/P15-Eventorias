03cd07905e53de95f2a86893f6d28988d5dfde12
Laury PRIN <prinlauryy@gmail.com>
2026-06-03 18:14:06 +0200

    EventListScreen.kt OK

--- app/src/main/java/com/openclassroom/eventorias/features/events/eventList/EventListScreen.kt
+++ app/src/main/java/com/openclassroom/eventorias/features/events/eventList/EventListScreen.kt
@@ -1,7 +1,9 @@
-package com.openclassroom.eventorias.features.events.homelist
+package com.openclassroom.eventorias.features.events.eventList
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Sort
 import androidx.compose.material.icons.filled.Add
@@ -10,11 +12,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.openclassroom.eventorias.R
+import com.openclassroom.eventorias.features.events.eventList.model.ListEventUiModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -23,19 +30,29 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 @Destination<RootGraph>
 @Composable
 fun EventListScreen(
-    navigator: DestinationsNavigator
+    navigator: DestinationsNavigator,
+    modifier: Modifier = Modifier
 ) {
+    val listEvent : List<ListEventUiModel> = emptyList() // TODO Brancher au viewModel
+
     Scaffold(
+        modifier = modifier,
         topBar = {
             TopAppBar(
-                title = { "Event List" },
+                title = { Text(stringResource(R.string.event_list_screen_title)) },
+                windowInsets = WindowInsets(0, 0, 0, 0),
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground,
+                    actionIconContentColor = MaterialTheme.colorScheme.onBackground,
+                ),
                 actions = {
                     IconButton(
                         onClick = { TODO() },
                     ) {
                         Icon(
                             imageVector = Icons.Default.Search,
-                            contentDescription = "Search"
+                            contentDescription = stringResource(R.string.search_button_description)
                         )
                     }
                     IconButton(
@@ -43,27 +60,30 @@ fun EventListScreen(
                     ) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.Sort,
-                            contentDescription = "Sort"
+                            contentDescription = stringResource(R.string.sort_button_description)
                         )
                     }
                 }
             )
         },
         floatingActionButton = {
-            FloatingActionButton(onClick = { TODO() }) {
+            FloatingActionButton(
+                onClick = { TODO() },
+                containerColor = MaterialTheme.colorScheme.primary
+            ) {
                 Icon(
                     imageVector = Icons.Default.Add,
-                    contentDescription = "Add Event"
+                    contentDescription = stringResource(R.string.add_fab_description)
                 )
             }
         },
     ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text("This is an event")
-            Text("This is an other event")
-            Text("This is a third event")
+        LazyColumn(modifier.padding(innerPadding)) {
+            items(listEvent){ event ->
+                EventItem(uiEvent = event, onEventClick = {TODO()})
+            }
         }
+
     }
-
-
 }
+
