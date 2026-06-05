ad4c3a35970dc10f36901914f5bd630c4a903886
Laury PRIN <prinlauryy@gmail.com>
2026-06-05 16:14:56 +0200

    MAJ du test du usecase du au amélioration faite pendant le développement de US 2.2

--- app/src/test/java/com/openclassroom/eventorias/features/event/useCases/GetUiEventListUseCaseTest.kt
+++ app/src/test/java/com/openclassroom/eventorias/features/event/useCases/GetUiEventListUseCaseTest.kt
@@ -9,9 +9,11 @@ import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class GetUiEventListUseCaseTest {
@@ -22,23 +24,42 @@ class GetUiEventListUseCaseTest {
     private val useCase = GetUiEventListUseCase(eventRepository, userRepository)
 
     @Test
-    fun `given events exist, when invoke, then maps with correct promoter avatars`() = runTest {
+    fun `given events exist, when invoke, then maps with correct promoter avatars and returns success`() = runTest {
         // GIVEN :
         val fakeEvent = Event(id = "event_1", title = "Concert", promoterId = "promoter_A")
         val fakeUser = User(id = "promoter_A", avatar = "https://url_de_l_avatar.png")
 
         every { eventRepository.getListEvent() } returns flowOf(listOf(fakeEvent))
-
         coEvery { userRepository.getCurrentUser("promoter_A") } returns fakeUser
 
         // WHEN :
-        val resultList = useCase().first()
+        val result = useCase().first()
 
         // THEN :
-        assertEquals(1, resultList.size)
+        assertTrue(result.isSuccess)
 
-        val uiModel = resultList.first()
-        assertEquals("Concert", uiModel.event.title)
-        assertEquals("https://url_de_l_avatar.png", uiModel.promoterAvatarUrl)
+        val uiList = result.getOrNull()
+        assertEquals(1, uiList?.size)
+
+        val uiModel = uiList?.first()
+        assertEquals("Concert", uiModel?.event?.title)
+        assertEquals("https://url_de_l_avatar.png", uiModel?.promoterAvatarUrl)
+    }
+
+    @Test
+    fun `given repository throws exception, when invoke, then returns failure Result`() = runTest {
+        // GIVEN :
+        val errorMessage = "Database error"
+        every { eventRepository.getListEvent() } returns flow {
+            throw Exception(errorMessage)
+        }
+
+        // WHEN :
+        val result = useCase().first()
+
+        // THEN :
+        assertTrue(result.isFailure)
+        val exception = result.exceptionOrNull()
+        assertEquals(errorMessage, exception?.message)
     }
 }
