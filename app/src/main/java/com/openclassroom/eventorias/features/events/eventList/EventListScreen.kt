94118282138d51c503c5a5434fed39b08988bbd9
Laury PRIN <prinlauryy@gmail.com>
2026-06-08 14:30:04 +0200

    Ajout de la fonctionnalité de recherche par titre

--- app/src/main/java/com/openclassroom/eventorias/features/events/eventList/EventListScreen.kt
+++ app/src/main/java/com/openclassroom/eventorias/features/events/eventList/EventListScreen.kt
@@ -3,13 +3,19 @@ package com.openclassroom.eventorias.features.events.eventList
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.Sort
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -20,10 +26,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.openclassroom.eventorias.R
@@ -45,33 +60,84 @@ fun EventListScreen(
 ) {
     val listEventState by viewModel.uiState.collectAsStateWithLifecycle()
 
+    val searchQuery by viewModel.searchQuery.collectAsStateWithLifecycle()
+
+    var isSearchBarActive by remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
+
+
+    LaunchedEffect(isSearchBarActive) {
+        if (isSearchBarActive) focusRequester.requestFocus()
+    }
+
     Scaffold(
         modifier = modifier,
         topBar = {
             TopAppBar(
-                title = { Text(stringResource(R.string.event_list_screen_title)) },
+                title = {
+                    // When in active search mode, a text field, and when no active search, the title of the screen/
+                    if (isSearchBarActive) {
+                        CustomSearchBar(
+                            searchQuery = searchQuery,
+                            onQueryChanged = { viewModel.setSearchQuery(it) },
+                            focusRequester = focusRequester
+                        )
+                    } else Text(stringResource(R.string.event_list_screen_title))
+                },
                 windowInsets = WindowInsets(0, 0, 0, 0),
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.background,
                     titleContentColor = MaterialTheme.colorScheme.onBackground,
                     actionIconContentColor = MaterialTheme.colorScheme.onBackground,
                 ),
-                actions = {
-                    IconButton(
-                        onClick = { TODO() },
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Search,
-                            contentDescription = stringResource(R.string.search_button_description)
-                        )
+                // Icon only display in active search mode to close the search bar
+                navigationIcon = {
+                    if (isSearchBarActive) {
+                        IconButton(onClick = {
+                            viewModel.setSearchQuery("")
+                            isSearchBarActive = false
+                        }
+                        ) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = stringResource(R.string.close_searchbar_description)
+                            )
+                        }
                     }
-                    IconButton(
-                        onClick = { TODO() }
-                    ) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.Sort,
-                            contentDescription = stringResource(R.string.sort_button_description)
-                        )
+                },
+                actions = {
+                    // When the search isn't active the buttons are the search button and the filter button
+                    if (!isSearchBarActive) {
+                        IconButton(
+                            onClick = { isSearchBarActive = true },
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Search,
+                                contentDescription = stringResource(R.string.search_button_description)
+                            )
+                        }
+
+                        IconButton(
+                            onClick = { TODO() }
+                        ) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.Sort,
+                                contentDescription = stringResource(R.string.sort_button_description)
+                            )
+                        }
+                    } else {
+                        // When the search is active, the button delete the active query
+                        IconButton(
+                            onClick = {
+                                if (searchQuery.isNotEmpty()) viewModel.setSearchQuery("")
+                                else isSearchBarActive = false
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.Close,
+                                contentDescription = stringResource(R.string.delete_search_description)
+                            )
+                        }
                     }
                 }
             )
@@ -114,5 +180,42 @@ fun EventListScreen(
             }
         }
     }
+}
+
+@Composable
+fun CustomSearchBar(
+    searchQuery: String,
+    onQueryChanged: (String) -> Unit,
+    focusRequester: FocusRequester
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    BasicTextField(
+        value = searchQuery,
+        onValueChange = { onQueryChanged(it) },
+        textStyle = MaterialTheme.typography.bodyLarge.copy(
+            color = MaterialTheme.colorScheme.onBackground
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .focusRequester(focusRequester),
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                keyboardController?.hide()
+                focusRequester.freeFocus()
+            }
+        ),
+        cursorBrush = SolidColor(MaterialTheme.colorScheme.onBackground),
+        decorationBox = { innerTextField ->
+            if (searchQuery.isEmpty()) {
+                Text(
+                    text = stringResource(R.string.search_label),
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f)
+                )
+            }
+            innerTextField()
+        }
+    )
 }
 
--- app/src/main/java/com/openclassroom/eventorias/features/events/eventList/EventListViewModel.kt
+++ app/src/main/java/com/openclassroom/eventorias/features/events/eventList/EventListViewModel.kt
@@ -8,10 +8,12 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
@@ -21,6 +23,9 @@ import kotlin.collections.emptyList
 class EventListViewModel @Inject constructor(eventListUseCase: GetUiEventListUseCase) :
     ViewModel() {
 
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery : StateFlow<String> = _searchQuery
+
     private val refreshSignal = MutableSharedFlow<Unit>(replay = 1).apply {
         tryEmit(Unit)
     }
@@ -28,18 +33,27 @@ class EventListViewModel @Inject constructor(eventListUseCase: GetUiEventListUse
     @OptIn(ExperimentalCoroutinesApi::class)
     val uiState: StateFlow<ListEventUiState> = refreshSignal
         .flatMapLatest {
-            eventListUseCase()
-                .map { result ->
-                    if (result.isSuccess) {
-                        val list = result.getOrNull() ?: emptyList()
-                        ListEventUiState.Success(listEvent = list)
+            combine(
+                eventListUseCase(),
+                _searchQuery.debounce(200)
+            ) { result, searchQuery ->
+                if (result.isSuccess) {
+                    val list = result.getOrNull() ?: emptyList()
+                    val filteredList = if (searchQuery.isNotBlank()) {
+                        list.filter { uiEvent ->
+                            uiEvent.event.title.contains(searchQuery, ignoreCase = true)
+                        }
                     } else {
-                        val exception = result.exceptionOrNull()
-                        ListEventUiState.Error(
-                            message = exception?.localizedMessage ?: "An error occured"
-                        )
+                        list
                     }
+                    ListEventUiState.Success(listEvent = filteredList)
+                } else {
+                    val exception = result.exceptionOrNull()
+                    ListEventUiState.Error(
+                        message = exception?.localizedMessage ?: "An error occurred"
+                    )
                 }
+            }
         }
         .onStart { emit(ListEventUiState.Loading) }
         .stateIn(
@@ -52,4 +66,7 @@ class EventListViewModel @Inject constructor(eventListUseCase: GetUiEventListUse
         viewModelScope.launch { refreshSignal.emit(Unit) }
     }
 
+    fun setSearchQuery(newQuery: String) {
+        _searchQuery.value = newQuery
+    }
 }
