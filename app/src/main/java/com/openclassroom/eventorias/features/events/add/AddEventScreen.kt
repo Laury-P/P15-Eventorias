565aa81be744c8aca74673c4027347fdbad9f2e3
Laury PRIN <prinlauryy@gmail.com>
2026-06-16 16:30:59 +0200

    Création de l'écran d'ajout d'événement et configuration de la navigation depuis la liste d'événements

---
+++ app/src/main/java/com/openclassroom/eventorias/features/events/add/AddEventScreen.kt
@@ -0,0 +1,12 @@
+package com.openclassroom.eventorias.features.events.add
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootGraph
+
+@Destination<RootGraph>
+@Composable
+fun AddEventScreen(){
+Text(text = "creation sceen")
+}
