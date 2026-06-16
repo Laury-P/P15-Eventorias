1cb78c6d6ae998f6b5279f0d4520da4ff065cdef
Laury PRIN <prinlauryy@gmail.com>
2026-06-16 11:36:11 +0200

    Fix utilisation du fichier string plutot que string en dur pour les onglet de navigation

--- app/src/main/java/com/openclassroom/eventorias/MainActivity.kt
+++ app/src/main/java/com/openclassroom/eventorias/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.openclassroom.eventorias.core.ui.theme.EventoriasTheme
@@ -60,7 +61,7 @@ class MainActivity : ComponentActivity() {
                                             contentDescription = null
                                         )
                                     },
-                                    label = { Text("Events") }
+                                    label = { Text(stringResource(R.string.event_nav_button)) }
                                 )
 
                                 // Onglet Profile
@@ -73,7 +74,7 @@ class MainActivity : ComponentActivity() {
                                             contentDescription = null
                                         )
                                     },
-                                    label = { Text("Profile") }
+                                    label = { Text(stringResource(R.string.profile_nav_button)) }
                                 )
                             }
                     }
